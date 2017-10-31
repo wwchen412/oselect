0d4ff8155462f29927b816a360c4d040cf2725ef
wwchen <wwchen412@gmail.com>
2017-10-31 17:59:21 +0800

    logo

--- Content/js/all.js
+++ Content/js/all.js
@@ -59,39 +59,39 @@ $('header .member').click(function() {
         })
     })
     // hover NAVBAR 底線
-$(function() {
 
-    var $el, leftPos, newWidth,
-        $mainNav = $("#mainNav");
 
-    $mainNav.append("<li id='magic-line'></li>");
-    var $magicLine = $("#magic-line");
+var $el, leftPos, newWidth,
+    $mainNav = $("#mainNav");
 
-    $magicLine
-        .width($(".current_page_item").width())
-        .css("left", $(".current_page_item").position().left)
-        .data("origLeft", $magicLine.position().left)
-        .data("origWidth", $magicLine.width());
+$mainNav.append("<li id='magic-line'></li>");
+var $magicLine = $("#magic-line");
 
-    $("#mainNav li").hover(function() {
-        $el = $(this);
-        leftPos = $el.position().left;
-        console.log(leftPos);
-        newWidth = $el.width();
-        $magicLine.stop().animate({
-            left: leftPos,
-            width: newWidth
-        });
+$magicLine
+    .width($(".current_page_item").width())
+    .css("left", $(".current_page_item").position().left)
+    .data("origLeft", $magicLine.position().left)
+    .data("origWidth", $magicLine.width());
 
-    }, function() {
-        $magicLine.stop().animate({
-            left: $magicLine.data("origLeft"),
-            width: $magicLine.data("origWidth")
-        });
+$("#mainNav li").hover(function() {
+    $el = $(this);
+    leftPos = $el.position().left;
+    console.log(leftPos);
+    newWidth = $el.width();
+    $magicLine.stop().animate({
+        left: leftPos,
+        width: newWidth
+    });
 
+}, function() {
+    $magicLine.stop().animate({
+        left: $magicLine.data("origLeft"),
+        width: $magicLine.data("origWidth")
     });
 
 });
+
+
 // menubar
 $('.menu-bar').click(function() {
     var phone_nav = $('.nav-phone');
@@ -99,17 +99,20 @@ $('.menu-bar').click(function() {
     if (phone_nav.hasClass('d-none')) {
         phone_nav.removeClass('d-none');
         body.addClass('active');
+        $('.logo a').css('z-index', 0);
     }
     phone_nav.find('span.close').click(function() {
         event.stopPropagation();
         $(this).parents('.nav-phone').addClass('d-none');
         body.removeClass('active');
+        $('.logo a').css('z-index', 3);
     })
     phone_nav.click(function() {
         event.stopPropagation();
         if (!$(this).hasClass('d-none')) {
             $(this).addClass('d-none');
             body.removeClass('active');
+            $('.logo a').css('z-index', 3);
         }
     })
     phone_nav.children().click(function() {
@@ -117,65 +120,3 @@ $('.menu-bar').click(function() {
     })
 
 })
-jQuery.extend(jQuery.validator.messages, {
-    required: "此欄位必填.",
-    remote: "Please fix this field.",
-    email: "請輸入正確的 Email 信箱.",
-    url: "請輸入正確的網址.",
-    date: "請輸入正確的日期.",
-    dateISO: "請輸入正確的 (ISO) 日期格式.",
-    number: "本欄位請填入數字.",
-    digits: "本欄位請填入數字.",
-    creditcard: "請輸入正確的信用卡號.",
-    equalTo: "密碼驗證失敗",
-    maxlength: $.validator.format("至多輸入 {0} 個字."),
-    minlength: $.validator.format("至少輸入 {0} 個字."),
-    rangelength: $.validator.format("請輸入 {0} 到 {1} 個字."),
-    range: $.validator.format("請輸入 {0} 到 {1} 的數字."),
-    max: $.validator.format("請輸入小於等於 {0} 的值."),
-    min: $.validator.format("請輸入大於等於 {0} 的值.")
-});
-
-jQuery.validator.addMethod("chkPid", function(value, element) {
-    var people_id = value.replace(/\s+/g, "");
-    return (
-        this.optional(element) || /^[A-Z]{1}[1-2]{1}[0-9]{8}$/.test(people_id)
-    );
-}, "請輸入正確格式,身分證字號開頭需為大寫英文");
-
-jQuery.validator.addMethod("mobileTaiwan", function(value, element) {
-    var str = value;
-    var result = false;
-    if (str.length > 0) {
-        //是否只有數字;
-        var patt_mobile = /^[\d]{1,}$/;
-        result = patt_mobile.test(str);
-
-        if (result) {
-            //檢查前兩個字是否為 09
-            //檢查前四個字是否為 8869
-            var firstTwo = str.substr(0, 2);
-            var firstFour = str.substr(0, 4);
-            var afterFour = str.substr(4, str.length - 1);
-            if (firstFour == '8869') {
-                $(element).val('09' + afterFour);
-                if (afterFour.length == 8) {
-                    result = true;
-                } else {
-                    result = false;
-                }
-            } else if (firstTwo == '09') {
-                if (str.length == 10) {
-                    result = true;
-                } else {
-                    result = false;
-                }
-            } else {
-                result = false;
-            }
-        }
-    } else {
-        result = true;
-    }
-    return result;
-}, "手機號碼不符合格式，僅允許09開頭的10碼數字");
